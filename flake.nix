10a7fad3dc8873eec3808c5b33ace0cc7876298f
luis <ldesiqueira@pinger.com>
2024-08-26 20:16:00 -0300

    init

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "A very basic flake with Home Manager";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager }: {
+
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+
+    homeConfigurations = {
+      myuser = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        modules = [
+          home-manager.modules.home-manager
+          ./home.nix
+        ];
+      };
+    };
+
+    nixosConfigurations = {
+      mysystem = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+        ];
+      };
+    };
+  };
+}
