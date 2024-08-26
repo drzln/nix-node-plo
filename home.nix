10b9d98f801e0da00a8a0a6c9760a0d6ff293365
luis <ldesiqueira@pinger.com>
2024-08-26 20:17:46 -0300

    init

---
+++ home.nix
@@ -0,0 +1,14 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "luis"; # Replace with your actual username
+  home.homeDirectory = "/home/luis"; # Replace with your home directory
+
+  # Set the state version to match Home Manager version
+  home.stateVersion = "24.05"; # Use the correct version for your setup
+
+  # Enable some basic packages
+  home.packages = with pkgs; [
+    vim # Text editor
+  ];
+}
