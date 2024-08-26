b8d1044615927f35bd0084699431d8481fb9dcde
luis <ldesiqueira@pinger.com>
2024-08-26 20:45:39 -0300

    init

--- configuration.nix
+++ configuration.nix
@@ -76,6 +76,13 @@
   services.openssh.enable = true;
   services.openssh.settings.PermitRootLogin = "yes";
   services.openssh.settings.PasswordAuthentication = true;
+  services.dnsmasq.enable = true;
+  services.dnsmasq.settings.server = [
+    "1.1.1.1"
+    "1.0.0.1"
+    "8.8.8.8"
+    "8.8.4.4"
+  ];
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
