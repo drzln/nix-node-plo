4efe1ce496a8ce99eaa6761e3be69b8407e2bc69
luis <ldesiqueira@pinger.com>
2024-08-31 00:06:06 -0300

    init

--- flake.nix
+++ flake.nix
@@ -11,25 +11,26 @@
 
   outputs = { self, nixpkgs, home-manager }: {
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+    # packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+    # packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
 
-    homeConfigurations = {
-      luis = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux;
-        modules = [
-          home-manager.modules.home-manager
-          ./home.nix
-        ];
-      };
-    };
+    # homeConfigurations = {
+    #   luis = home-manager.lib.homeManagerConfiguration {
+    #     pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    #     modules = [
+    #       home-manager.modules.home-manager
+    #       ./home.nix
+    #     ];
+    #   };
+    # };
 
     nixosConfigurations = {
       plo = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           /etc/nixos/configuration.nix
+          ./configuration.nix
           home-manager.nixosModules.home-manager
         ];
       };
