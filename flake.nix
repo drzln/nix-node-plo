7c684cee017e3840acb8a002823a9ca249b85be9
Luis De Siqueira <luis@pleme.io>
2024-09-01 16:08:09 -0300

    a great commit

--- flake.nix
+++ flake.nix
@@ -2,48 +2,75 @@
   description = "blackmatter systems";
 
   inputs = {
-    hyprland = {
-      url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
-    };
+    ############################################################################
+    # base modules
+    ############################################################################
+
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    # end base modules
+
+    ############################################################################
+    # utilities
+    ############################################################################
+
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+
+    # end utilities
+
+    ############################################################################
+    # desktop modules
+    ############################################################################
+
+    hyprland = {
+      url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
+    };
+
+    # end desktop modules
   };
 
   outputs = { 
     self, 
     nixpkgs, 
     home-manager, 
+    flake-utils,
     hyprland, 
-    ... }@inputs: {
-    inherit self.outputs;
+    ... }@inputs: 
 
-    # packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+    flake-utils.lib.eachSystem ["x86_64-linux" "aarch64-linux" "x86_64-darwin" ] (system:
+      let
+        outputs = {
+          inherit self nixpkgs home-manager hyprland;
 
-    # packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+          homeConfigurations = {
+            luis = home-manager.lib.homeManagerConfiguration {
+              extraSpecialArgs = { inherit inputs outputs; };
+              pkgs = nixpkgs.legacyPackages.x86_64-linux;
+              modules = [
+                ./home.nix
+              ];
+            };
+          };
 
-    homeConfigurations = {
-      luis = home-manager.lib.homeManagerConfiguration {
-        extraSpecialArgs = { inherit inputs outputs; };
-        pkgs = nixpkgs.legacyPackages.x86_64-linux;
-        modules = [
-          ./home.nix
-        ];
-      };
-    };
+          nixosConfigurations = {
+            plo = nixpkgs.lib.nixosSystem {
+              specialArgs = { inherit inputs outputs; };
+              inherit system;
+              modules = [
+                /etc/nixos/configuration.nix
+                ./configuration.nix
+                home-manager.nixosModules.home-manager
+              ];
+            };
+          };
 
-    nixosConfigurations = {
-      plo = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs outputs; };
-        system = "x86_64-linux";
-        modules = [
-          /etc/nixos/configuration.nix
-          ./configuration.nix
-          home-manager.nixosModules.home-manager
-        ];
-      };
-    };
-  };
+        };
+      in
+    outputs);
 }
