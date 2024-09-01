adb9977bb429b1c850baf191a8794301b5bc3cbe
Luis De Siqueira <luis@pleme.io>
2024-09-01 15:36:12 -0300

    a great commit

--- home.nix
+++ home.nix
@@ -177,5 +177,7 @@
 
   wayland.windowManager.hyprland = {
     enable = true;
+    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
   };
 }
