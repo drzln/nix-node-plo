0fa6f79b71c0535c5807197e51f2efbfe989483a
luis <ldesiqueira@pinger.com>
2024-09-01 20:03:10 -0300

    init

--- flake.nix
+++ flake.nix
@@ -17,12 +17,11 @@
     let
       requirements = {
         inherit inputs;
-        outputs = self;
       };
 
       homeConfigurations = {
         luis = home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          pkgs = import nixpkgs { inherit system; };
           modules = [
             ./home.nix
           ];
