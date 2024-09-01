f1f991fce6d0cc998f1cf5213af0778970c9a57d
Luis De Siqueira <luis@pleme.io>
2024-09-01 14:43:11 -0300

    a great commit

--- configuration.nix
+++ configuration.nix
@@ -1,6 +1,15 @@
 { config, pkgs, inputs, ... }:
-
+let
+  pkgs-unstable = inputs.hyprland.inputs.nixpkgs.legacyPackages.${pkgs.stdenv.hostPlatform.system};
+in
 {
+  hardware.opengl = {
+    package = pkgs-unstable.mesa.drivers;
+
+    # if you also want 32-bit support (e.g for Steam)
+    driSupport32Bit = true;
+    package32 = pkgs-unstable.pkgsi686Linux.mesa.drivers;
+  };
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
