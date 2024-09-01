220f77a79a37a137350a21383becfec87dec4c3c
Luis De Siqueira <luis@pleme.io>
2024-09-01 04:08:07 -0300

    a great commit

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
       luis = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux;
         modules = [
-          home-manager.modules.home-manager
+          home-manager.nixosModules.home-manager
           ./home.nix
         ];
       };
