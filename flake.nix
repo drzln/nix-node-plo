61522e5d49555600c910b756c8b424d6385ef74d
Luis De Siqueira <luis@pleme.io>
2024-09-01 15:49:46 -0300

    a great commit

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
     home-manager, 
     hyprland, 
     ... }@inputs: {
-    inherit (self) outputs;
+    inherit outputs;
 
     # packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
 
