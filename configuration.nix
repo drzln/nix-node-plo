e1b481b05b32f4c7b4584f56bfda0e3c630bb624
luis <ldesiqueira@pinger.com>
2024-09-01 19:56:46 -0300

    init

--- configuration.nix
+++ configuration.nix
@@ -7,6 +7,7 @@
     ./xserver.nix
   ];
 
+  home-manager.users.luis = requirements.output.homeConfigurations.luis;
   # home-manager.users.luis = import ./home.nix;
   # home-manager.backupFileExtension = "backup";
 
