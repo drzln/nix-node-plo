2da6bd64a96381900e36d08f792bd8d6b0832545
Luis De Siqueira <luis@pleme.io>
2024-09-01 14:26:39 -0300

    a great commit

--- configuration.nix
+++ configuration.nix
@@ -211,7 +211,10 @@
     };
     settings = { 
       auto-optimise-store = true;
-      #substituters = [];
+      substituters = [
+        "https://hyprland.cachix.org"
+      ];
+      trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
     };
     package = pkgs.nixFlakes;
   };
