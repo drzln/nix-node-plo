97351167c0398c7f856d25f0cd84ef982f7eb247
Luis De Siqueira <luis@pleme.io>
2024-09-02 01:30:02 -0300

    a great commit

--- nodes/plo/configuration.nix
+++ nodes/plo/configuration.nix
@@ -176,45 +176,48 @@ in
 
   fonts.fontconfig.enable = false;
   environment.etc."fonts/fonts.conf".text = ''
-     <?xml version="1.0"?>
-     <!DOCTYPE fontconfig SYSTEM "fonts.dtd">
-     <fontconfig>
-       <!-- I BE HERE-->
+    <?xml version="1.0"?>
+    <!DOCTYPE fontconfig SYSTEM "fonts.dtd">
+    <fontconfig>
+      
+      <!-- Add cachedir elements -->
+      <cachedir>/var/cache/fontconfig</cachedir>
+      <cachedir prefix="xdg">fontconfig</cachedir>
+      
+      <!-- Enable antialiasing for smoother font rendering -->
+      <match target="font">
+        <edit name="antialias" mode="assign">
+          <bool>true</bool>
+        </edit>
+      </match>
 
-       <!-- Enable antialiasing for smoother font rendering -->
-       <match target="font">
-         <edit name="antialias" mode="assign">
-           <bool>true</bool>
-         </edit>
-       </match>
+      <!-- Enable subpixel rendering -->
+      <match target="font">
+        <edit name="rgba" mode="assign">
+          <const>rgb</const>
+        </edit>
+      </match>
 
-       <!-- Enable subpixel rendering -->
-       <match target="font">
-         <edit name="rgba" mode="assign">
-           <const>rgb</const>
-         </edit>
-       </match>
+      <!-- Set hinting to slight for better font clarity -->
+      <match target="font">
+        <edit name="hinting" mode="assign">
+          <bool>true</bool>
+        </edit>
+        <edit name="hintstyle" mode="assign">
+          <const>hintslight</const>
+        </edit>
+      </match>
 
-       <!-- Set hinting to slight for better font clarity -->
-       <match target="font">
-         <edit name="hinting" mode="assign">
-           <bool>true</bool>
-         </edit>
-         <edit name="hintstyle" mode="assign">
-           <const>hintslight</const>
-         </edit>
-       </match>
+      <!-- Default font substitution -->
+      <match target="pattern">
+        <test qual="any" name="family">
+          <string>monospace</string>
+        </test>
+        <edit name="family" mode="assign">
+          <string>DejaVu Sans Mono</string>
+        </edit>
+      </match>
 
-       <!-- Default font substitution -->
-       <match target="pattern">
-         <test qual="any" name="family">
-           <string>monospace</string>
-         </test>
-         <edit name="family" mode="assign">
-           <string>DejaVu Sans Mono</string>
-         </edit>
-       </match>
-
-     </fontconfig>
+    </fontconfig>
   '';
 }
