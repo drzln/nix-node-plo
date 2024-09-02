f323671dd01e6476fe4de3cb34686a38c80dbf09
Luis De Siqueira <luis@pleme.io>
2024-09-02 14:33:33 -0300

    a great commit

--- nodes/plo/boot.nix
+++ nodes/plo/boot.nix
@@ -1,5 +1,4 @@
 { configs, pkgs, inputs, outputs, ... }: {
-  security.selinux.enable = false;
   boot.kernelParams = [ "nvidia-drm.modeset=1" "selinux=0" ];
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
