3019a85184ab1f47d7b5636f4d7db1c225fdf1c2
Luis De Siqueira <luis@pleme.io>
2024-09-02 16:24:40 -0300

    a great commit

--- nodes/plo/docker.nix
+++ nodes/plo/docker.nix
@@ -1,14 +1,14 @@
 {...}: {
   systemd.services.docker.serviceConfig = {
-    LimitNOFILE = 1048576;
-    LimitNPROC = 65536;
+    LimitNOFILE = "1048576";
+    LimitNPROC = "65536";
     LimitSTACK = "infinity";
     LimitMEMLOCK = "infinity";
   };
 
   systemd.user.services.docker.serviceConfig = {
-    LimitNOFILE = 1048576;
-    LimitNPROC = 65536;
+    LimitNOFILE = "1048576";
+    LimitNPROC = "65536";
     LimitSTACK = "infinity";
     LimitMEMLOCK = "infinity";
   };
