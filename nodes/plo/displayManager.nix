0391c747a73f661bccb1dbc003312dbfe97798fc
Luis De Siqueira <luis@pleme.io>
2024-09-02 02:23:55 -0300

    a great commit

--- nodes/plo/displayManager.nix
+++ nodes/plo/displayManager.nix
@@ -1,19 +1,24 @@
 { ... }: {
-  services.xserver.displayManager.setupCommands = ''
-    mkdir -p /etc/sddm/scripts
-    echo "#!/bin/sh" > /etc/sddm/scripts/Xsetup
-    echo "xrandr --output DP-2 --mode 1920x1080 --rate 360" >> /etc/sddm/scripts/Xsetup
-    chmod +x /etc/sddm/scripts/Xsetup
-  '';
+  # services.xserver.displayManager.setupCommands = ''
+  #   mkdir -p /etc/sddm/scripts
+  #   echo "#!/bin/sh" > /etc/sddm/scripts/Xsetup
+  #   echo "xrandr --output DP-2 --mode 1920x1080 --rate 360" >> /etc/sddm/scripts/Xsetup
+  #   chmod +x /etc/sddm/scripts/Xsetup
+  # '';
   services.displayManager = {
     defaultSession = "gnome";
     sddm = {
       enable = true;
       theme = "nord";
       wayland.enable = true;
-      # settings = {
-      #   HaltCommand="/etc/sddm/scripts/xsetup";
-      # };
+      settings = {
+        Wayland = {
+          ServerArguments = "--refresh 360":
+        };
+        X11 = {
+          ServerArguments = "--refresh 360";
+        };
+      };
     };
   };
 }
