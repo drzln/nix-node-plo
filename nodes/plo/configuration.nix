9ea70a909d6dac1fedf83aeb0416b320ab7aae02
Luis De Siqueira <luis@pleme.io>
2024-09-02 00:58:02 -0300

    a great commit

--- nodes/plo/configuration.nix
+++ nodes/plo/configuration.nix
@@ -63,7 +63,7 @@ in
 
   xdg.portal.enable = true;
   xdg.portal.wlr.enable = true;
-  xdg.portal.extraPortals = [pkgs.xdg-desktop-portal-gtk];
+  # xdg.portal.extraPortals = [pkgs.xdg-desktop-portal-gtk];
 
   users.users.luis = {
     uid = 1001;
