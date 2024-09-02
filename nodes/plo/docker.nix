86ce571a5c19edda0cff36f05bfc74b55ff55bdf
Luis De Siqueira <luis@pleme.io>
2024-09-02 16:26:50 -0300

    a great commit

--- nodes/plo/docker.nix
+++ nodes/plo/docker.nix
@@ -6,17 +6,17 @@
     LimitMEMLOCK = "infinity";
   };
 
-  systemd.user.services.docker.serviceConfig = {
-    LimitNOFILE = "1048576";
-    LimitNPROC = "65536";
-    LimitSTACK = "infinity";
-    LimitMEMLOCK = "infinity";
-  };
+  # systemd.user.services.docker.serviceConfig = {
+  #   LimitNOFILE = 1048576;
+  #   LimitNPROC = 65536;
+  #   LimitSTACK = "infinity";
+  #   LimitMEMLOCK = "infinity";
+  # };
 
-  # systemd.extraConfig = ''
-  #   DefaultLimitNOFILE=1048576
-  #   DefaultLimitNPROC=65536
-  #   DefaultLimitSTACK=infinity
-  #   DefaultLimitMEMLOCK=infinity
-  # '';
+  systemd.extraConfig = ''
+    DefaultLimitNOFILE=1048576
+    DefaultLimitNPROC=65536
+    DefaultLimitSTACK=infinity
+    DefaultLimitMEMLOCK=infinity
+  '';
 }
