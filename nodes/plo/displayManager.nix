06c5c1549fde66391b75d51ef757bef8466010d1
Luis De Siqueira <luis@pleme.io>
2024-09-02 02:13:22 -0300

    a great commit

--- nodes/plo/displayManager.nix
+++ nodes/plo/displayManager.nix
@@ -5,6 +5,16 @@
       enable = true;
       theme = "nord";
       wayland.enable = true;
+      extraConfig = ''
+        [General]
+        HaltCommand=/etc/sddm/scripts/Xsetup
+      '';
+      extraConfigCommands = ''
+        mkdir -p /etc/sddm/scripts
+        echo "#!/bin/sh" > /etc/sddm/scripts/Xsetup
+        echo "xrandr --output DP-2 --mode 1920x1080 --rate 360" >> /etc/sddm/scripts/Xsetup
+        chmod +x /etc/sddm/scripts/Xsetup
+      '';
     };
   };
 }
