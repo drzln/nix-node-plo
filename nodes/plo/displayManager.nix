fe1f2549f5237164f9f7c2332ffeacaf280356f6
Luis De Siqueira <luis@pleme.io>
2024-09-02 02:17:17 -0300

    a great commit

--- nodes/plo/displayManager.nix
+++ nodes/plo/displayManager.nix
@@ -1,12 +1,12 @@
 { ... }: {
+  services.xserver.displayManager.setupCommands = ''
+    mkdir -p /etc/sddm/scripts
+    echo "#!/bin/sh" > /etc/sddm/scripts/Xsetup
+    echo "xrandr --output DP-2 --mode 1920x1080 --rate 360" >> /etc/sddm/scripts/Xsetup
+    chmod +x /etc/sddm/scripts/Xsetup
+  '';
   services.displayManager = {
     defaultSession = "gnome";
-    setupCommands = ''
-      mkdir -p /etc/sddm/scripts
-      echo "#!/bin/sh" > /etc/sddm/scripts/Xsetup
-      echo "xrandr --output DP-2 --mode 1920x1080 --rate 360" >> /etc/sddm/scripts/Xsetup
-      chmod +x /etc/sddm/scripts/Xsetup
-    '';
     sddm = {
       enable = true;
       theme = "nord";
