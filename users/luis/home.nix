9d93cec9b34a9dbb5ae1003082511ffeb8ba9c3c
Luis De Siqueira <luis@pleme.io>
2024-09-01 21:08:55 -0300

    a great commit

--- users/luis/home.nix
+++ users/luis/home.nix
@@ -180,13 +180,14 @@
       bind = "SUPER+SHIFT+Q, exec, hyprctl dispatch exit";
     };
   };
+
   wayland.windowManager.hyprland = {
     enable = true;
     package = requirements.inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
     # portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
-    settings = {
-      "$mod" = "SUPER";
-      bind = ["SUPER+SHIFT+Q, exec, hyprctl dispatch exit"];
-    };
+    # settings = {
+    #   "$mod" = "SUPER";
+    #   bind = ["SUPER+SHIFT+Q, exec, hyprctl dispatch exit"];
+    # };
   };
 }
