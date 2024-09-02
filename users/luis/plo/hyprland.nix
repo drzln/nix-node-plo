1e945f98d2f5422bfa7616055d49ad915cb39ea6
Luis De Siqueira <luis@pleme.io>
2024-09-02 00:08:46 -0300

    a great commit

--- users/luis/plo/hyprland.nix
+++ users/luis/plo/hyprland.nix
@@ -14,7 +14,7 @@
     enable = true;
     package = requirements.inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
     settings = {
-      # monitor = "DP-2,preferred,360";
+      monitor = "DP-2, 1920x1080@360, 0x0, 1";
       bind = [
         "SUPER_SHIFT, Q, exec, hyprctl dispatch exit"
         "SUPER_SHIFT, T, exec, foot"
