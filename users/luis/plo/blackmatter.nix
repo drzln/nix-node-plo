81f6f0cf4d38ade5a2c9a60448f37239602f08d3
Luis De Siqueira <luis@pleme.io>
2024-09-02 21:19:52 -0300

    a great commit

--- users/luis/plo/blackmatter.nix
+++ users/luis/plo/blackmatter.nix
@@ -3,6 +3,7 @@
     requirements.outputs.homeManagerModules.blackmatter
   ];
   blackmatter.programs.nvim.enable = true;
+  # blackmatter.programs.nvim.package = true;
   blackmatter.shell.enable = true;
   blackmatter.gitconfig.enable = false;
   blackmatter.desktop.enable = true;
