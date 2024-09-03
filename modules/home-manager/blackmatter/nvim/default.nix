a01bf5aa9123ad90086f4fc66c49953678229e73
Luis De Siqueira <luis@pleme.io>
2024-09-03 08:43:02 -0300

    a great commit

--- modules/home-manager/blackmatter/nvim/default.nix
+++ modules/home-manager/blackmatter/nvim/default.nix
@@ -17,7 +17,7 @@ let
     lsp.enable = true;
     completion.enable = true;
     debugging.enable = true;
-    treesitter.enable = true;
+    treesitter.enable = false;
     telescope.enable = true;
   };
 
@@ -164,7 +164,7 @@ let
       wellle."targets.vim".enable = true;
       ggandor."leap.nvim".enable = true;
       unblevable.quick-scope.enable = true;
-      t3rro.nvim-ts-rainbow.enable = true;
+      t3rro.nvim-ts-rainbow.enable = false;
 
       # snippets
       L3MON4D3.LuaSnip.enable = true;
