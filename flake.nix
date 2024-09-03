3fe5da92a11ca0ba484f5843324df304d640df38
Luis De Siqueira <luis@pleme.io>
2024-09-02 22:38:50 -0300

    a great commit

--- flake.nix
+++ flake.nix
@@ -31,6 +31,8 @@
         inherit inputs outputs;
       };
 
+      overlays = import ./overlays;
+
       specialArgs = { inherit requirements; };
       extraSpecialArgs = specialArgs;
 
@@ -40,8 +42,7 @@
         "luis@plo" = home-manager.lib.homeManagerConfiguration {
           inherit extraSpecialArgs;
           pkgs = import nixpkgs { 
-            overlays = import ./overlays;
-            inherit system; 
+            inherit system overlays; 
           };
           modules = [
             ./users/luis/plo
