548917bfc0af52531942b007bda6b4a9846111c2
Luis De Siqueira <luis@pleme.io>
2024-09-03 08:26:16 -0300

    a great commit

--- overlays/neovim.nix
+++ overlays/neovim.nix
@@ -1,17 +1,28 @@
-# neovim.nix
-{ stdenv, fetchurl }:
+{ lib, stdenv, fetchFromGitHub, cmake, ninja, libtool, pkgconfig, gettext, lua, msgpack, libuv, tree-sitter, unzip, callPackage }:
 
+# Import the luv derivation from the file
+let
+  luv = callPackage ./luv.nix { inherit lua libuv cmake; };
+in
 stdenv.mkDerivation rec {
-  pname = "neovim";
+  pname = "neovim_raw";
   version = "0.8.0";
 
-  src = fetchurl {
-    url = "https://github.com/neovim/neovim/releases/download/v${version}/nvim-linux64.tar.gz";
-    sha256 = "sha256-GvJ0cfdvG0961lY8hjpKeBF/BRXjOQ7k2RETKXBRf6c=";
+  src = fetchFromGitHub {
+    owner = "neovim";
+    repo = "neovim";
+    rev = "v${version}";
+    sha256 = "your-correct-sha256-hash";  # Replace this with the actual sha256 hash for v0.8.0
   };
 
-  installPhase = ''
-    mkdir -p $out/bin
-    tar -xzf $src -C $out/bin --strip-components=1
-  '';
+  nativeBuildInputs = [ cmake ninja libtool pkgconfig gettext unzip ];
+
+  buildInputs = [ lua luv msgpack libuv tree-sitter ];
+
+  cmakeFlags = [
+    "-DCMAKE_BUILD_TYPE=Release"
+    "-DCMAKE_INSTALL_PREFIX=$out"
+    "-DLIBLUV_INCLUDE_DIR=${luv.dev}/include"
+    "-DLIBLUV_LIBRARY=${luv}/lib/libluv.so"
+  ];
 }
