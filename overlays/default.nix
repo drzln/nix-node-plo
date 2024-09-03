4ab04e19d25c9fcd50685572f8f96254661559a8
Luis De Siqueira <luis@pleme.io>
2024-09-02 21:26:38 -0300

    a great commit

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,11 +1,13 @@
 [
-  neovim = super.neovim.overrideAttrs (oldAttrs: rec {
-    version = "0.8.0";
-    src = super.fetchFromGitHub {
-      owner = "neovim";
-      repo = "neovim";
-      rev = "v${version}";
-      sha256 = "1szx648cffsaqqnm0j9jy5ibky20nfdzbfd5v7i989pw8f79amwr";
-    };
-  });
+  (self: super: {
+    neovim = super.neovim.overrideAttrs (oldAttrs: rec {
+      version = "0.8.0";
+      src = super.fetchFromGitHub {
+        owner = "neovim";
+        repo = "neovim";
+        rev = "v${version}";
+        sha256 = "1szx648cffsaqqnm0j9jy5ibky20nfdzbfd5v7i989pw8f79amwr";
+      };
+    });
+  })
 ]
