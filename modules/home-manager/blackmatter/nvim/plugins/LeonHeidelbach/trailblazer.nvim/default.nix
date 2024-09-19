21b051fb0907236c0365de66788bca795a6dd428
Luis De Siqueira <luis@pleme.io>
2024-09-18 21:01:19 -0300

    LeonHeidelback/trailblazer.nvim update

--- modules/home-manager/blackmatter/nvim/plugins/LeonHeidelbach/trailblazer.nvim/default.nix
+++ modules/home-manager/blackmatter/nvim/plugins/LeonHeidelbach/trailblazer.nvim/default.nix
@@ -5,7 +5,7 @@ let
   name = "trailblazer.nvim";
   url = "https://github.com/${author}/${name}";
   ref = "main";
-  rev = "8f49c735bfa3aa47bbb8fb1bd586df2a77028a92";
+  rev = "674bb6254a376a234d0d243366224122fc064eab";
   plugPath = ".local/share/nvim/site/pack/${author}/start/${name}";
   cfg = config.blackmatter.programs.nvim.plugins.${author}.${name};
 in
