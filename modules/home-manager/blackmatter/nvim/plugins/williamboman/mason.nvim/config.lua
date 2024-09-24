735a6f60cd61711a34409236e1c57be81e5f0760
Luis De Siqueira <luis@pleme.io>
2024-09-24 02:09:14 -0300

    tsserver -> ts_ls

--- modules/home-manager/blackmatter/nvim/plugins/williamboman/mason.nvim/config.lua
+++ modules/home-manager/blackmatter/nvim/plugins/williamboman/mason.nvim/config.lua
@@ -1,12 +1,24 @@
 local M = {}
 function M.setup()
-  require("mason").setup()
-  require("mason-lspconfig").setup(
+  local mason = require("mason")
+  local mason_lspconfig = require("mason-lspconfig")
+
+  mason.setup()
+
+  local available_servers = mason_lspconfig.get_available_servers()
+
+  mason_lspconfig.setup(
     {
-      ensure_installed = { "rust_analyzer" },
-      automatic_installation = false
+      ensure_installed = available_servers,
+      automatic_installation = true
     }
   )
+
+  local lspconfig = require("lspconfig")
+
+  for _, server in ipairs(available_servers) do
+      lspconfig[server].setup {}
+  end
 end
 
 return M
