04c5f8b028b69269e627cd453d6d72b071070c4e
Luis De Siqueira <luis@pleme.io>
2024-09-24 03:02:26 -0300

    install as many language servers as possible

--- modules/home-manager/blackmatter/nvim/plugins/williamboman/mason.nvim/config.lua
+++ modules/home-manager/blackmatter/nvim/plugins/williamboman/mason.nvim/config.lua
@@ -7,17 +7,62 @@ function M.setup()
 
   local available_servers = mason_lspconfig.get_available_servers()
 
+  local exclude_servers = {
+      "erlangls",
+      "teal_ls",
+      "foam_ls",
+      "asm_lsp",
+      "beancount",
+      "standardrb",
+      "salt_ls",
+      "java_language_server",
+      "move_analyzer",
+      "rubocop",
+      "r_language_server",
+      "ruby_ls",
+      "theme_check",
+      "solargraph",
+      "clarity_lsp",
+      "awk_ls",
+      "nimls",
+      "tsserver",
+      "ocamllsp",
+      "csharp_ls",
+      "vala_ls",
+      "vuels",
+      "hls",
+      "als"
+  }
+
+  local function is_excluded(server)
+    return vim.tbl_contains(exclude_servers, server)
+  end
+
+  local servers_to_install = vim.tbl_filter(function(server)
+      return not is_excluded(server)
+  end, available_servers)
+
   mason_lspconfig.setup(
     {
-      ensure_installed = available_servers,
-      automatic_installation = true
+      ensure_installed = servers_to_install,
+      automatic_installation = false
     }
   )
 
+
   local lspconfig = require("lspconfig")
 
+  local server_configs = {}
   for _, server in ipairs(available_servers) do
-      lspconfig[server].setup {}
+    if not is_excluded(server) then
+      local success, err = pcall(function()
+          local config = server_configs[server] or {}
+          lspconfig[server].setup(config)
+      end)
+      if not success then
+        print("Error setting up " .. server .. ": " .. err)
+      end
+    end
   end
 end
 
