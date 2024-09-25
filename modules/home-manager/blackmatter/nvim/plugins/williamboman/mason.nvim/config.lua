91fc0d10fe02aa1e305d83d66471c5f69a5a2517
luis <ldesiqueira@pinger.com>
2024-09-25 12:46:01 -0300

    init

--- modules/home-manager/blackmatter/nvim/plugins/williamboman/mason.nvim/config.lua
+++ modules/home-manager/blackmatter/nvim/plugins/williamboman/mason.nvim/config.lua
@@ -8,30 +8,30 @@ function M.setup()
   local available_servers = mason_lspconfig.get_available_servers()
 
   local exclude_servers = {
-      "custom_elements_ls",
-      "nil_ls",
-      "sorbet",
-      "sourcery",
-      "erlangls",
-      "teal_ls",
-      "foam_ls",
-      "asm_lsp",
-      "beancount",
-      "salt_ls",
-      "java_language_server",
-      "move_analyzer",
-      "r_language_server",
-      "ruby_ls",
-      "clarity_lsp",
-      "awk_ls",
-      "nimls",
-      "tsserver",
-      "ocamllsp",
-      "csharp_ls",
-      "vala_ls",
-      "vuels",
-      "hls",
-      "als"
+    "custom_elements_ls",
+    "nil_ls",
+    "sorbet",
+    "sourcery",
+    "erlangls",
+    "teal_ls",
+    "foam_ls",
+    "asm_lsp",
+    "beancount",
+    "salt_ls",
+    "java_language_server",
+    "move_analyzer",
+    "r_language_server",
+    "ruby_ls",
+    "clarity_lsp",
+    "awk_ls",
+    "nimls",
+    "tsserver",
+    "ocamllsp",
+    "csharp_ls",
+    "vala_ls",
+    "vuels",
+    "hls",
+    "als"
   }
 
   local function is_excluded(server)
@@ -39,7 +39,7 @@ function M.setup()
   end
 
   local servers_to_install = vim.tbl_filter(function(server)
-      return not is_excluded(server)
+    return not is_excluded(server)
   end, available_servers)
 
   mason_lspconfig.setup(
@@ -65,8 +65,8 @@ function M.setup()
   for _, server in ipairs(available_servers) do
     if not is_excluded(server) then
       local success, err = pcall(function()
-          local config = server_configs[server] or {}
-          lspconfig[server].setup(config)
+        local config = server_configs[server] or {}
+        lspconfig[server].setup(config)
       end)
       if not success then
         print("Error setting up " .. server .. ": " .. err)
