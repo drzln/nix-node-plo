1299a87cbc0a6652dd97982e1981776bd24443d7
Luis De Siqueira <luis@pleme.io>
2024-09-27 19:52:28 -0300

    nord desktop

--- users/luis/plo/blackmatter.nix
+++ users/luis/plo/blackmatter.nix
@@ -9,7 +9,7 @@
   blackmatter.desktop.enable = true;
   blackmatter.desktop.monitors = {
     main = {
-      name = "DP-2";      # Secondary monitor output name
+      name = "DP-1";      # Secondary monitor output name
       mode = "1920x1080"; # Resolution of the monitor
       rate = "360";       # Refresh rate in Hz for a higher refresh secondary monitor
     };
