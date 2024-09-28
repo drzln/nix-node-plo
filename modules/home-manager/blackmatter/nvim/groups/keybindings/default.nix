75f26dbfedca3b24d23bef693fd77f691f1efe11
Luis De Siqueira <luis@pleme.io>
2024-09-27 23:37:13 -0300

    migrate telescope under common

---
+++ modules/home-manager/blackmatter/nvim/groups/keybindings/default.nix
@@ -0,0 +1,46 @@
+{ config, lib, pkgs, ... }:
+with lib;
+let
+  cfg = config.blackmatter.programs.nvim.plugin.groups.keybindings;
+  configPath = "${common.baseConfigPath}/groups/keybindings.lua";
+in
+{
+  options.blackmatter.programs.nvim.plugin.groups.keybindings =
+    {
+      enable = mkEnableOption "keybindings";
+    };
+
+  # imports = [
+  #   ../../groups/telescope
+  #   ../../groups/treesitter
+  #   ../../plugins/jcdickinson/http.nvim
+  #   ../../plugins/nvim-lua/plenary
+  #   ../../plugins/folke/which-key.nvim
+  #   ../../plugins/folke/neodev.nvim
+  #   ../../plugins/maaslalani/nordbuddy
+  #   ../../plugins/numToStr/Comment
+  #   ../../plugins/nathom/filetype
+  # ];
+
+  config =
+    mkMerge [
+      (mkIf cfg.enable
+        {
+					home.file."${configPath}".source = ./config.lua;
+					# blackmatter.programs.nvim.plugin.groups.telescope.enable = true;
+					# blackmatter.programs.nvim.plugin.groups.treesitter.enable = true;
+          # blackmatter.programs.nvim.plugins =
+          #   {
+          #     numToStr.Comment.enable = true;
+          #     folke."neodev.nvim".enable = true;
+          #     jcdickinson."http.nvim".enable = false;
+          #     nathom.filetype.enable = true;
+          #     nvim-lua.plenary.enable = true;
+          #
+          #     folke."which-key.nvim".enable = false;
+          #     maaslalani.nordbuddy.enable = false;
+          #   };
+        }
+      )
+    ];
+}
