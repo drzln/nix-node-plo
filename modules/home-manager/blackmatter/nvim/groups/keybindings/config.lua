62653af4d1738fc05892bc2108f2b7aee2246750
Luis De Siqueira <luis@pleme.io>
2024-09-28 00:29:11 -0300

    theming

--- modules/home-manager/blackmatter/nvim/groups/keybindings/config.lua
+++ modules/home-manager/blackmatter/nvim/groups/keybindings/config.lua
@@ -1,29 +1,29 @@
 local M = {}
 
 function M.setup()
-	local opts = { noremap = true, silent = true }
+  local opts = { noremap = true, silent = true }
 
-	-- Set up keybinding helper function
-	local keymap = vim.api.nvim_set_keymap
+  -- Normal mode keybindings
+  vim.keymap.set('n', '<C-h>', '<C-w>h', opts)  -- Move to left window
+  vim.keymap.set('n', '<C-j>', '<C-w>j', opts)  -- Move to below window
+  vim.keymap.set('n', '<C-k>', '<C-w>k', opts)  -- Move to above window
+  vim.keymap.set('n', '<C-l>', '<C-w>l', opts)  -- Move to right window
 
-	-- Normal mode keybindings
-	keymap('n', '<C-h>', '<C-w>h', opts)  -- Move to left window
-	keymap('n', '<C-j>', '<C-w>j', opts)  -- Move to below window
-	keymap('n', '<C-k>', '<C-w>k', opts)  -- Move to above window
-	keymap('n', '<C-l>', '<C-w>l', opts)  -- Move to right window
+  -- Telescope keybindings (plugin-dependent)
+  vim.keymap.set('n', '<Leader>fi', '<Cmd>Telescope find_files<CR>', opts)
+  vim.keymap.set('n', '<Leader>fg', '<Cmd>Telescope live_grep<CR>', opts)
+  vim.keymap.set('n', '<Leader>fb', '<Cmd>Telescope buffers<CR>', opts)
+  vim.keymap.set('n', '<Leader>fh', '<Cmd>Telescope help_tags<CR>', opts)
+  vim.keymap.set('n', '<Leader>gb', '<Cmd>Telescope git_branches<CR>', opts)
+  vim.keymap.set('n', '<Leader>gc', '<Cmd>Telescope git_commits<CR>', opts)
+  vim.keymap.set('n', '<Leader>gr', '<Cmd>Telescope lsp_references<CR>', opts)
+  vim.keymap.set('n', '<Leader>gd', '<Cmd>Telescope lsp_definitions<CR>', opts)
+  vim.keymap.set('n', '<Leader>fc', '<Cmd>Telescope command_history<CR>', opts)
+  vim.keymap.set('n', '<Leader>fk', '<Cmd>Telescope keymaps<CR>', opts)
+  vim.keymap.set('n', '<Leader>fs', '<Cmd>Telescope search_history<CR>', opts)
 
-	-- Telescope keybindings (plugin-dependent)
-	keymap('n', '<Leader>fi', '<Cmd>Telescope find_files<CR>', opts)
-	keymap('n', '<Leader>fg', '<Cmd>Telescope live_grep<CR>', opts)
-	keymap('n', '<Leader>fb', '<Cmd>Telescope buffers<CR>', opts)
-	keymap('n', '<Leader>fh', '<Cmd>Telescope help_tags<CR>', opts)
-	keymap('n', '<Leader>gb', '<Cmd>Telescope git_branches<CR>', opts)
-	keymap('n', '<Leader>gc', '<Cmd>Telescope git_commits<CR>', opts)
-	keymap('n', '<Leader>gr', '<Cmd>Telescope lsp_references<CR>', opts)
-	keymap('n', '<Leader>gd', '<Cmd>Telescope lsp_definitions<CR>', opts)
-	keymap('n', '<Leader>fc', '<Cmd>Telescope command_history<CR>', opts)
-	keymap('n', '<Leader>fk', '<Cmd>Telescope keymaps<CR>', opts)
-	keymap('n', '<Leader>fs', '<Cmd>Telescope search_history<CR>', opts)
+  -- Map 'ff' to format the buffer in normal mode
+  vim.keymap.set('n', 'ff', vim.lsp.buf.format, opts)
 end
 
 return M
