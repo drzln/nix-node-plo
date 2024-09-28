c3dc7ec6707792ea5d441b96e1d7bdb821fde1cd
Luis De Siqueira <luis@pleme.io>
2024-09-27 21:59:17 -0300

    theming

--- modules/home-manager/blackmatter/nvim/plugins/shaunsingh/nord.nvim/default.nix
+++ modules/home-manager/blackmatter/nvim/plugins/shaunsingh/nord.nvim/default.nix
@@ -4,19 +4,20 @@ let
 	author = "shaunsingh";
 	name = "nord.nvim";
 	plugName = "nord";
-  cfg = config.blackmatter.programs.nvim.plugins.shaunsingh.nord;
+  cfg = config.blackmatter.programs.nvim.plugins.${author}.${name};
   common = import ../../../common;
   configPath = "${common.baseConfigPath}/${author}/${plugName}.lua";
+  plugPath = "${common.basePlugPath}/${author}/start/${name}";
   ref = "master";
   rev = import ./rev.nix;
   url = "https://github.com/shaunsingh/nord.nvim";
 in
 {
-  options.blackmatter.programs.nvim.plugins.shaunsingh."nord".enable = mkEnableOption "shaunsingh/nord";
+  options.blackmatter.programs.nvim.plugins.${author}.${name}.enable = mkEnableOption "${author}/${name}";
 
   config = mkMerge [
     (mkIf cfg.enable {
-      home.file.".local/share/nvim/site/pack/shaunsingh/start/nord.nvim".source =
+      home.file."${plugPath}".source =
         builtins.fetchGit { inherit ref rev url;};
       home.file."${configPath}".source = ./config.lua;
     })
