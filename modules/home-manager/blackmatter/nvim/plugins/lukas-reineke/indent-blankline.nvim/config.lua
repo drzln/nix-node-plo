b0d84aaaa11e99e446d59ae9e8f6cd6c0ceafffa
Luis De Siqueira <luis@pleme.io>
2024-09-30 04:51:51 -0300

    indent blankline

--- modules/home-manager/blackmatter/nvim/plugins/lukas-reineke/indent-blankline.nvim/config.lua
+++ modules/home-manager/blackmatter/nvim/plugins/lukas-reineke/indent-blankline.nvim/config.lua
@@ -1,21 +1,11 @@
 local M = {}
 function M.setup()
-	require('lualine').setup({
-		options = {
-			theme = 'nord',
-			section_separators = { '', '' },
-			component_separators = { '', '' },
-			icons_enabled = true,
-		},
-		sections = {
-			lualine_a = { 'mode' },
-			lualine_b = { 'branch', 'diff', 'diagnostics' },
-			lualine_c = { 'filename' },
-			lualine_x = { 'encoding', 'fileformat', 'filetype' },
-			lualine_y = { 'progress' },
-			lualine_z = { 'location' }
-		},
-	});
+	require('indent_blankline').setup {
+		char = '▏',
+		show_trailing_blankline_indent = false,
+		show_current_context = true,
+		show_current_context_start = true,
+	}
 end
 
 return M
