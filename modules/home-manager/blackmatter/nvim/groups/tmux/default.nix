5f9e791bda741e66f21c1c440dc32b88c8829315
Luis De Siqueira <luis@pleme.io>
2024-09-30 03:08:35 -0300

    theming

---
+++ modules/home-manager/blackmatter/nvim/groups/tmux/default.nix
@@ -0,0 +1,46 @@
+{ config, lib, pkgs, ... }:
+with lib;
+let
+  cfg = config.blackmatter.programs.nvim.plugin.groups.tmux;
+in
+{
+  options.blackmatter.programs.nvim.plugin.groups.tmux =
+    {
+      enable = mkEnableOption "tmux";
+    };
+
+  imports = [
+    # ../../groups/telescope
+    # ../../groups/treesitter
+    # ../../groups/keybindings
+    # ../../plugins/jcdickinson/http.nvim
+    # ../../plugins/nvim-lua/plenary
+    # ../../plugins/folke/which-key.nvim
+    # ../../plugins/folke/neodev.nvim
+    # ../../plugins/maaslalani/nordbuddy
+    # ../../plugins/numToStr/Comment
+    # ../../plugins/nathom/filetype
+  ];
+
+  config =
+    mkMerge [
+      (mkIf cfg.enable
+        {
+          # blackmatter.programs.nvim.plugin.groups.telescope.enable = true;
+          # blackmatter.programs.nvim.plugin.groups.treesitter.enable = true;
+          # blackmatter.programs.nvim.plugin.groups.keybindings.enable = true;
+          #      blackmatter.programs.nvim.plugins =
+          #        {
+          #          numToStr.Comment.enable = true;
+          #          folke."neodev.nvim".enable = true;
+          #          jcdickinson."http.nvim".enable = false;
+          #          nathom.filetype.enable = true;
+          #          nvim-lua.plenary.enable = true;
+          #
+          #          folke."which-key.nvim".enable = false;
+          #          maaslalani.nordbuddy.enable = false;
+          #        };
+        }
+      )
+    ];
+}
---
+++ modules/home-manager/blackmatter/nvim/plugins/christoomey/vim-tmux-navigator/default.nix
@@ -0,0 +1,25 @@
+{ lib, config, ... }:
+with lib;
+let
+  cfg = config.blackmatter.programs.nvim.plugins.${author}.${name};
+  common = import ../../../common;
+  url = "${common.baseRepoUrl}/${author}/${name}";
+  plugPath = "${common.basePlugPath}/${author}/start/${name}";
+  # configPath = "${common.baseConfigPath}/${author}/${plugName}.lua";
+  ref = "main";
+  author = "christoomey";
+  name = "vim-tmux-navigator";
+  plugName = name;
+  rev = import ./rev.nix;
+in
+{
+  options.blackmatter.programs.nvim.plugins.${author}.${name}.enable =
+    mkEnableOption "${author}/${name}";
+
+  config = mkMerge [
+    (mkIf cfg.enable {
+      home.file."${plugPath}".source =
+        builtins.fetchGit { inherit ref rev url; };
+    })
+  ];
+}
