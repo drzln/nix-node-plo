7df5b08f26c72396b3f33b6ffa824e20d9836e23
Luis De Siqueira <luis@pleme.io>
2024-09-30 03:56:04 -0300

    project.vnim

--- modules/home-manager/blackmatter/nvim/plugins/nvim-telescope/telescope.nvim/default.nix
+++ modules/home-manager/blackmatter/nvim/plugins/nvim-telescope/telescope.nvim/default.nix
@@ -1,20 +1,26 @@
 { lib, config, ... }:
 with lib;
 let
-  cfg = config.blackmatter.programs.nvim.plugins.nvim-telescope."telescope.nvim";
+  cfg = config.blackmatter.programs.nvim.plugins.${author}.${name};
+  url = "${common.baseRepoUrl}/${author}/${name}";
+  common = import ../../../common;
+  plugPath = "${common.basePlugPath}/${author}/start/${name}";
+  configPath = "${common.baseConfigPath}/${author}/${plugName}.lua";
+  rev = import ./rev.nix;
+  ref = "master";
+  author = "nvim-telescope";
+  name = "telescope.nvim";
+  plugName = "telescope";
 in
 {
-  options.blackmatter.programs.nvim.plugins.nvim-telescope."telescope.nvim".enable =
-    mkEnableOption "nvim-telescope/telescope.nvim";
+  options.blackmatter.programs.nvim.plugins.${author}.${name}.enable =
+    mkEnableOption "${author}/${name}";
 
   config = mkMerge [
     (mkIf cfg.enable {
-      home.file.".local/share/nvim/site/pack/nvim-telescope/start/telescope.nvim".source =
-        builtins.fetchGit {
-          url = "https://github.com/nvim-telescope/telescope.nvim";
-          ref = "master";
-          rev = import ./rev.nix;
-        };
+      home.file."${plugPath}".source =
+        builtins.fetchGit { inherit ref rev url; };
+      home.file."${configPath}".source = ./config.lua;
     })
   ];
 }
