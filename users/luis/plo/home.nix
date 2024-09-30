6c70fdc3e09c6d0aab2b157ce9c4e60b63fe1ff3
luis <luis@pleme.io>
2024-09-30 06:20:00 -0300

    add gpg

--- users/luis/plo/home.nix
+++ users/luis/plo/home.nix
@@ -60,6 +60,7 @@
   services.gpg-agent.enable = true;
   services.gpg-agent.pinentryPackage = pkgs.pinentry-curses;
   services.gpg-agent.extraConfig = ''
-		allow-curses-pinentry
-	'';
+    default-cache-ttl 600
+    max-cache-ttl 7200
+  '';
 }
