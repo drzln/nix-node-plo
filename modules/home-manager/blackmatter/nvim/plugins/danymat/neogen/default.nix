4c94cd08abc4469e49564c9fc24d867751c19996
gaby <gab@pleme.io>
2024-10-01 15:29:10 -0300

    d plugins standardized

--- modules/home-manager/blackmatter/nvim/plugins/danymat/neogen/default.nix
+++ modules/home-manager/blackmatter/nvim/plugins/danymat/neogen/default.nix
@@ -1,13 +1,15 @@
 { lib, config, ... }:
 with lib;
 let
+	cfg = config.blackmatter.programs.nvim.plugins.${author}.${name};
+  common = import ../../../common;
+  url = "${common.baseRepoUrl}/${author}/${name}";
+  plugPath = "${common.basePlugPath}/${author}/start/${name}";
+  configPath = "${common.baseConfigPath}/${author}/${plugName}.lua";
   author = "danymat";
   name = "neogen";
-  url = "https://github.com/${author}/${name}";
   ref = "main";
   rev = import ./rev.nix;
-  plugPath = ".local/share/nvim/site/pack/${author}/start/${name}";
-  cfg = config.blackmatter.programs.nvim.plugins.${author}.${name};
 in
 {
   options.blackmatter.programs.nvim.plugins.${author}.${name}.enable =
