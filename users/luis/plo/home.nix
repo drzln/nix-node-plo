7c761443def996bbefcf3addd3f44b7e8a308aaa
luis <luis@pleme.io>
2024-10-03 01:39:11 -0300

    shadeflakes

--- users/luis/plo/home.nix
+++ users/luis/plo/home.nix
@@ -7,6 +7,7 @@
     ./hyprland.nix
     ./blackmatter.nix
     ./secrets
+    ./shadeflakes
   ];
   home.stateVersion = "24.05";
   home.username = "luis";
--- users/luis/plo/shadeflakes/default.nix
+++ users/luis/plo/shadeflakes/default.nix
@@ -5,7 +5,7 @@
     	'';
 
   home.file."code/codecommit/flake.nix" = {
-    source = ./code/codecommmit/flake.nix;
+    source = ./code/codecommit/flake.nix;
   };
 
   home.file."code/codecommit/.envrc" = {
