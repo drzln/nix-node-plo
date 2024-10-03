6ba732aa215dec87d20e90eb839fb5ee00c9eacd
luis <luis@pleme.io>
2024-10-03 01:26:10 -0300

    shadeflakes

--- users/luis/plo/shadeflakes/default.nix
+++ users/luis/plo/shadeflakes/default.nix
@@ -1,6 +1,6 @@
 {
   home.file."code/codecommit/flake.nix" = {
-    source = "./code/codecommmit/flake.nix";
+    source = ./code/codecommmit/flake.nix;
   };
   home.file."code/codecommit/.envrc" = {
     text = "use flake";
