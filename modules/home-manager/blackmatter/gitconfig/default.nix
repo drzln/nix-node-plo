47ea7f5286c71cfe8674668b0e9e4a60e5ae2ed8
luis <luis@pleme.io>
2024-10-03 02:24:12 -0300

    shadeflakes

--- modules/home-manager/blackmatter/gitconfig/default.nix
+++ modules/home-manager/blackmatter/gitconfig/default.nix
@@ -23,29 +23,29 @@ in
   config = mkMerge [
     (mkIf cfg.enable {
       home.file.".gitconfig".text = ''
-        [user]
-        	email = ${cfg.email}
-        	name = ${cfg.user}
+                [user]
+                	email = ${cfg.email}
+                	name = ${cfg.user}
 
-				[init]
-					defaultBranch = main
+        				[init]
+        					defaultBranch = main
 
-				[push]
-					default = simple
+        				[push]
+        					default = simple
 
-				[branch "main"]
-					remote = origin
-					merge = refs/head/main
+        				[branch "main"]
+        					remote = origin
+        					merge = refs/head/main
 
-        [merge]
-        	default = merge
+                [merge]
+                	default = merge
 
-        [core]
-        	pager = delta --dark --line-numbers
-					editor = nvim
+                [core]
+                	pager = delta --dark --line-numbers
+        					editor = nvim
 
-        [delta]
-        	side-by-side = true
+                [delta]
+                	side-by-side = true
       '';
     })
   ];
