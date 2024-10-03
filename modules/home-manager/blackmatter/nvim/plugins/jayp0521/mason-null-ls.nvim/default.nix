6451b9a3371fed67703f94c696eb5364d7c7a659
luis <luis@pleme.io>
2024-10-03 12:03:53 -0300

    mason null-ls

---
+++ modules/home-manager/blackmatter/nvim/plugins/jayp0521/mason-null-ls.nvim/default.nix
@@ -0,0 +1,23 @@
+{ lib, config, ... }:
+with lib;
+let
+  cfg = config.blackmatter.programs.nvim.plugins.${author}.${name};
+  common = import ../../../common;
+  url = "${common.baseRepoUrl}/${author}/${name}";
+  plugPath = "${common.basePlugPath}/${author}/start/${name}";
+  author = "jayp0521";
+  name = "mason-null-ls.nvim";
+  ref = "main";
+  rev = import ./rev.nix;
+in
+{
+  options.blackmatter.programs.nvim.plugins.${author}.${name}.enable =
+    mkEnableOption "${author}/${name}";
+
+  config = mkMerge [
+    (mkIf cfg.enable {
+      home.file."${plugPath}".source =
+        builtins.fetchGit { inherit ref rev url; };
+    })
+  ];
+}
