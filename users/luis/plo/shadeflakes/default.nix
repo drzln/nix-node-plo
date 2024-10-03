09639457e9dfd79c3031c16acac97449f392dd25
luis <luis@pleme.io>
2024-10-03 01:37:44 -0300

    shadeflakes

--- users/luis/plo/shadeflakes/default.nix
+++ users/luis/plo/shadeflakes/default.nix
@@ -1,14 +1,14 @@
 { lib, ... }: {
-  # home.activation.createCodeCommitDir = lib.mkAfter ''
-  #   		mkdir -p $HOME/code/codecommit
-  #   		chmod 700 $HOME/code/codecommit
-  #   	'';
+  home.activation.createCodeCommitDir = lib.mkAfter ''
+    		mkdir -p $HOME/code/codecommit
+    		chmod 700 $HOME/code/codecommit
+    	'';
 
-  # home.file."code/codecommit/flake.nix" = {
-  #   source = ./code/codecommmit/flake.nix;
-  # };
+  home.file."code/codecommit/flake.nix" = {
+    source = ./code/codecommmit/flake.nix;
+  };
 
-  # home.file."code/codecommit/.envrc" = {
-  #   text = "use flake";
-  # };
+  home.file."code/codecommit/.envrc" = {
+    text = "use flake";
+  };
 }
