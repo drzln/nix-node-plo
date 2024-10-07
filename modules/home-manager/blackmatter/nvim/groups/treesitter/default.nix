7d73a36cea437da47c1c6054e56cf83ceca24c8b
luis <luis@pleme.io>
2024-10-07 09:53:32 -0300

    nvim-treesitter/playground

--- modules/home-manager/blackmatter/nvim/groups/treesitter/default.nix
+++ modules/home-manager/blackmatter/nvim/groups/treesitter/default.nix
@@ -11,6 +11,7 @@ in
 
   imports = [
     ../../plugins/nvim-treesitter/nvim-treesitter
+    ../../plugins/nvim-treesitter/playground
     ../../plugins/nvim-treesitter/nvim-treesitter-context
     ../../plugins/nvim-treesitter/nvim-treesitter-refactor
     ../../plugins/nvim-treesitter/nvim-treesitter-textobjects
@@ -23,6 +24,7 @@ in
           blackmatter.programs.nvim.plugins =
             {
               nvim-treesitter.nvim-treesitter.enable = true;
+              nvim-treesitter.playground.enable = true;
               nvim-treesitter.nvim-treesitter-context.enable = true;
               nvim-treesitter.nvim-treesitter-refactor.enable = false;
               nvim-treesitter.nvim-treesitter-textobjects.enable = false;
