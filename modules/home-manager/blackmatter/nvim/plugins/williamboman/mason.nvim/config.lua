b76acc9ea4ad6612afebeadb2b703530d4066a9d
luis <luis@pleme.io>
2024-10-07 11:27:26 -0300

    update dev endpoint role

--- modules/home-manager/blackmatter/nvim/plugins/williamboman/mason.nvim/config.lua
+++ modules/home-manager/blackmatter/nvim/plugins/williamboman/mason.nvim/config.lua
@@ -1,89 +1,90 @@
 local M = {}
 function M.setup()
-  local mason = require("mason")
-  local mason_lspconfig = require("mason-lspconfig")
+	local mason = require("mason")
+	local mason_lspconfig = require("mason-lspconfig")
 
-  mason.setup()
+	mason.setup()
 
-  local available_servers = mason_lspconfig.get_available_servers()
+	local available_servers = mason_lspconfig.get_available_servers()
 
-  local exclude_servers = {
-    "efm",
-    "diagnosticls",
-    "spectral",
-    "azure_pipelines_ls",
-    "tflint",
-    "remark_ls",
-    "marksman",
-    "prosemd_lsp",
-    "vale_ls",
-    "grammarly",
-    "custom_elements_ls",
-    "pkgbuild_language_server",
-    "nil_ls",
-    "sorbet",
-    "sourcery",
-    "erlangls",
-    "teal_ls",
-    "foam_ls",
-    "asm_lsp",
-    "beancount",
-    "salt_ls",
-    "java_language_server",
-    "move_analyzer",
-    "r_language_server",
-    "ruby_ls",
-    "clarity_lsp",
-    "awk_ls",
-    "nimls",
-    "tsserver",
-    "ocamllsp",
-    "csharp_ls",
-    "vala_ls",
-    "vuels",
-    "hls",
-    "als"
-  }
+	local exclude_servers = {
+		"efm",
+		"yammls",
+		"diagnosticls",
+		"spectral",
+		"azure_pipelines_ls",
+		"tflint",
+		"remark_ls",
+		"marksman",
+		"prosemd_lsp",
+		"vale_ls",
+		"grammarly",
+		"custom_elements_ls",
+		"pkgbuild_language_server",
+		"nil_ls",
+		"sorbet",
+		"sourcery",
+		"erlangls",
+		"teal_ls",
+		"foam_ls",
+		"asm_lsp",
+		"beancount",
+		"salt_ls",
+		"java_language_server",
+		"move_analyzer",
+		"r_language_server",
+		"ruby_ls",
+		"clarity_lsp",
+		"awk_ls",
+		"nimls",
+		"tsserver",
+		"ocamllsp",
+		"csharp_ls",
+		"vala_ls",
+		"vuels",
+		"hls",
+		"als"
+	}
 
-  local function is_excluded(server)
-    return vim.tbl_contains(exclude_servers, server)
-  end
+	local function is_excluded(server)
+		return vim.tbl_contains(exclude_servers, server)
+	end
 
-  local servers_to_install = vim.tbl_filter(function(server)
-    return not is_excluded(server)
-  end, available_servers)
+	local servers_to_install = vim.tbl_filter(function(server)
+		return not is_excluded(server)
+	end, available_servers)
 
-  mason_lspconfig.setup(
-    {
-      ensure_installed = servers_to_install,
-      automatic_installation = false
-    }
-  )
+	mason_lspconfig.setup(
+		{
+			ensure_installed = servers_to_install,
+			automatic_installation = false
+		}
+	)
 
 
-  local lspconfig = require("lspconfig")
+	local lspconfig = require("lspconfig")
 
-  local server_configs = {
-    nil_ls = {
-      settins = {
-        ['nil'] = {
-          autoArchive = true,
-        },
-      },
-    }
-  }
+	local server_configs = {
+		nil_ls = {
+			settins = {
+				['nil'] = {
+					autoArchive = true,
+				},
+			},
+		}
+	}
 
-  for _, server in ipairs(available_servers) do
-    if not is_excluded(server) then
-      local success, err = pcall(function()
-        local config = server_configs[server] or {}
-        lspconfig[server].setup(config)
-      end)
-      if not success then
-        print("Error setting up " .. server .. ": " .. err)
-      end
-    end
-  end
+	for _, server in ipairs(available_servers) do
+		if not is_excluded(server) then
+			local success, err = pcall(function()
+				local config = server_configs[server] or {}
+				lspconfig[server].setup(config)
+			end)
+			if not success then
+				print("Error setting up " .. server .. ": " .. err)
+			end
+		end
+	end
 end
 
 return M
