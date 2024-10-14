7c935d2d5fe16cf0ec0aa528da92354cfe106aac
luis <luis@pleme.io>
2024-10-14 15:00:28 -0300

    bufferline nord

--- modules/home-manager/blackmatter/nvim/plugins/akinsho/bufferline.nvim/config.lua
+++ modules/home-manager/blackmatter/nvim/plugins/akinsho/bufferline.nvim/config.lua
@@ -1,6 +1,38 @@
 local M = {}
 function M.setup()
 	require("bufferline").setup {
+		highlights = {
+			fill = {
+				bg = "#81A1C1",
+			},
+			background = {
+				bg = "#81A1C1",
+			},
+			tab = {
+				bg = "#81A1C1",
+			},
+			tab_selected = {
+				fg = "#ECEFF4",
+				bg = "#5E81AC",
+			},
+			tab_close = {
+				bg = "#81A1C1",
+			},
+			close_button = {
+				bg = "#81A1C1",
+			},
+			close_button_selected = {
+				bg = "#5E81AC",
+			},
+			buffer_visible = {
+				bg = "#81A1C1",
+			},
+			buffer_selected = {
+				fg = "#ECEFF4",
+				bg = "#5E81AC",
+				bold = true,
+			},
+		},
 		options = {
 			numbers = "buffer_id",
 			diagnostics = "nvim_lsp",
