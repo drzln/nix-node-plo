c33da7e2dda26b2ff371906e1928b4249e0f26cf
luis <ldesiqueira@pinger.com>
2024-10-15 15:32:02 -0300

    format overlays

--- nodes/cid/default.nix
+++ nodes/cid/default.nix
@@ -143,6 +143,7 @@
 
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
+  home-manager.backupFileExtension = "backup";
   home-manager.users.ldesiqueira =
     { outputs, ... }: {
       imports = [ ../../modules/home-manager/blackmatter ];
