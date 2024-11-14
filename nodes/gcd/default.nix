e50dacc52a23c93bdb546ef68ffa83ac11146e26
gab <gab@gmail.com>
2024-11-14 16:01:53 -0500

    nixos level

--- nodes/gcd/default.nix
+++ nodes/gcd/default.nix
@@ -1,3 +1,28 @@
-{
+{ pkgs, ... }: {
+  services.xserver.enable = true;
   services.xserver.videoDrivers = [ "intel" ];
+  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  # services.gnome = {
+  #   clocks.enable = true;
+  #   calendar.enable = true;
+  #   contacts.enable = true;
+  #   music.enable = false; # Disable GNOME Music if unnecessary
+  #   weather.enable = true;
+  # };
+
+  # Input settings (optional)
+  # services.xserver.layout = "us";
+  services.xserver.xkbOptions = "eurosign:e,ctrl:nocaps"; # Remap Caps Lock to Control
+
+  # Allow unfree software for proprietary GNOME extensions if needed
+  nixpkgs.config.allowUnfree = true;
+
+  # Optional system-wide packages
+  environment.systemPackages = with pkgs; [
+    # gnomeExtensions
+    # gnome-shell
+    # gnome-terminal
+    # dconf-editor
+  ];
 }
--- users/gabrielad/gcd/home.nix
+++ users/gabrielad/gcd/home.nix
@@ -12,52 +12,38 @@
   home.username = "gabrielad";
   home.homeDirectory = "/home/gabrielad";
 
-  # Enable XSession and configure GNOME as the window manager
+  # Enable XSession with GNOME as the window manager
   xsession.enable = true;
-  xsession.windowManager.gnome.enable = true;
+	#xsession.windowManager.gnome.enable = true;
 
-  # Enable dmenu for application launching
-  programs.dmenu.enable = true;
+  # Customize GNOME settings, including themes, icons, and keybindings
+  # services.gnome.gsettings = {
+  #   enable = true;
+  #
+  #   overrides = {
+  #     "/org/gnome/desktop/interface" = {
+  #       "gtk-theme" = "Adwaita-dark";
+  #       "icon-theme" = "Papirus-Dark";
+  #       "cursor-theme" = "Adwaita";
+  #     };
+  #
+  #     "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+  #       "binding" = "<Super>d";
+  #       "command" = "dmenu_run";
+  #       "name" = "Program Launcher (dmenu)";
+  #     };
+  #   };
+  # };
 
-  # Customize GNOME settings, including keybindings
-  programs.dconf.enable = true;
-  programs.dconf.settings = {
-    "/org/gnome/desktop/interface" = {
-      # Use dark theme for GTK applications
-      "gtk-theme" = "Adwaita-dark";
-
-      # Set Papirus-Dark as the icon theme
-      "icon-theme" = "Papirus-Dark";
-
-      # Use the default cursor theme
-      "cursor-theme" = "Adwaita";
-    };
-    "/org/gnome/settings-daemon/plugins/media-keys" = {
-      "custom-keybindings" = [
-        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
-      ];
-    };
-    "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
-      # Shortcut: Super (Windows key) + d
-      "binding" = "<Super>d";
-
-      # Open the application menu (dmenu)
-      "command" = "dmenu_run";
-
-      # Program launcher name
-      "name" = "Program Launcher (dmenu)";
-    };
-  };
-
-  # Configure GPG for secure encryption and signing
+  # GPG configuration for secure encryption and signing
   programs.gpg.enable = true;
-  services.gpg-agent.enable = true;
-  services.gpg-agent.pinentryPackage = pkgs.pinentry-curses;
-  services.gpg-agent.extraConfig = ''
-    # Cache passphrases for 600 seconds (10 minutes)
-    default-cache-ttl 600
 
-    # Allow caching for up to 7200 seconds (2 hours)
+  services.gpg-agent.enable = true;
+
+  services.gpg-agent.pinentryPackage = pkgs.pinentry-curses;
+
+  services.gpg-agent.extraConfig = ''
+    default-cache-ttl 600
     max-cache-ttl 7200
   '';
 }
