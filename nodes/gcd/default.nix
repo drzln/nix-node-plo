b1625dedb7c912f275a1afcd0139fc16da266fa1
gab <gab@gmail.com>
2024-11-19 12:47:53 -0300

    gcd settings

--- nodes/gcd/default.nix
+++ nodes/gcd/default.nix
@@ -4,17 +4,9 @@
   services.xserver.desktopManager.gnome.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.displayManager.autoLogin.enable = false;
-  # services.gnome = {
-  #   clocks.enable = true;
-  #   calendar.enable = true;
-  #   contacts.enable = true;
-  #   music.enable = false; # Disable GNOME Music if unnecessary
-  #   weather.enable = true;
-  # };
 
   # Input settings (optional)
-  # services.xserver.layout = "us";
-  services.xserver.xkbOptions = "eurosign:e,ctrl:nocaps"; # Remap Caps Lock to Control
+  services.xserver.xkb.options = "eurosign:e,ctrl:nocaps"; # Remap Caps Lock to Control
 
   # Allow unfree software for proprietary GNOME extensions if needed
   nixpkgs.config.allowUnfree = true;
