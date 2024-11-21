3ce59a2a81589a25a1b480188a19f83f5c0aa535
gab <gab@gmail.com>
2024-11-21 18:09:06 -0300

    gcd settings

--- nodes/gcd/default.nix
+++ nodes/gcd/default.nix
@@ -1,9 +1,16 @@
 { pkgs, ... }: {
   services.xserver.enable = true;
-  services.xserver.videoDrivers = [ "intel" ];
+  services.xserver.videoDrivers = [ "modsetting" ];
   services.xserver.desktopManager.gnome.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.displayManager.autoLogin.enable = false;
+  boot.kernelParams = [
+    "i915.enable_dpcd_backlight=3"
+    "acpi_backlight=vendor"
+  ];
+
+  # boot.kernelModules = [ "i915" ];
+  boot.kernelModules = [ "video" ];
 
   # Input settings (optional)
   services.xserver.xkb.options = "eurosign:e,ctrl:nocaps"; # Remap Caps Lock to Control
@@ -13,6 +20,11 @@
 
   # Optional system-wide packages
   environment.systemPackages = with pkgs; [
+    mesa
+    mesa-demos
+    vulkan-loader
+    vulkan-tools
+    intel-gpu-tools
     # gnomeExtensions
     # gnome-shell
     # gnome-terminal
