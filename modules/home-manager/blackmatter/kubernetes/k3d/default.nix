8218987b39f3c683bc398aec2c2bb02790eb9862
luis <luis@pleme.io>
2024-11-24 09:23:45 -0300

    ++ kubernetes module

--- modules/home-manager/blackmatter/kubernetes/k3d/default.nix
+++ modules/home-manager/blackmatter/kubernetes/k3d/default.nix
@@ -20,12 +20,50 @@ in
       default = [ ];
       description = "List of additional k3d clusters to configure. Each entry must have 'name', 'apiPort', and optional 'ports'.";
     };
+
+    client.enable = mkEnableOption "Enable client tools and KUBECONFIG management for k3d clusters";
+
+    client.tools = mkOption {
+      type = types.listOf types.str;
+      default = [ "kubectl" "k9s" ];
+      description = "List of client tools to install for interacting with Kubernetes clusters.";
+    };
   };
 
   config = mkIf cfg.enable {
     home.packages = with pkgs; [
       k3d # Ensure k3d is installed
+      # {optionalString cfg.client.enable (map (tool: pkgs.${tool}) cfg.client.tools)}
     ];
+
+    home.sessionVariables = lib.mkIf cfg.client.enable {
+      KUBECONFIG = "${config.home.homeDirectory}/.kube/config";
+    };
+
+    # Setup KUBECONFIG for default and additional clusters
+    home.file = lib.mkIf cfg.client.enable {
+      ".kube/config".text = builtins.concatStringsSep "\n"
+        (map
+          (cluster:
+            ''
+              apiVersion: v1
+              clusters:
+              - cluster:
+                  server: https://${if cluster.name == "default" then cfg.address else cluster.apiPort}
+                name: ${cluster.name}
+              contexts:
+              - context:
+                  cluster: ${cluster.name}
+                  user: ${cluster.name}-user
+                name: ${cluster.name}-context
+              current-context: ${if cluster.name == "default" then "default-context" else cluster.name + "-context"}
+              users:
+              - name: ${cluster.name}-user
+                user:
+                  token: dummy-token-for-${cluster.name}
+            '')
+          ([{ name = "default"; apiPort = cfg.address; }] ++ cfg.additionalClusters));
+    };
 
     # Systemd services for all k3d clusters (default and additional)
     systemd.user.services = lib.genAttrs
