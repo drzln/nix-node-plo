dd04833482842a9af06eb3bd8edffcbf94a9e1fc
luis <luis@pleme.io>
2024-11-25 20:35:36 -0300

    + taplo

--- modules/home-manager/blackmatter/nvim/conf/lua/lsp/config/rust_analyzer.lua
+++ modules/home-manager/blackmatter/nvim/conf/lua/lsp/config/rust_analyzer.lua
@@ -4,17 +4,10 @@ function M.setup(opts)
 	local lspconfig = require("lspconfig")
 
 	local rust_analyzer_opts = vim.tbl_extend("force", {
-		on_attach = function(client, _)
-			client.server_capabilities.documentFormattingProvider = false
-			client.server_capabilities.documentRangeFormattingProvider = false
-		end,
-		settings = {
-			["rust-analyzer"] = {
-				diagnostics = {
-					disabled = { "unresolved-proc-macro" }
-				},
-			}
-		}
+		rustfmt = {
+			enableRangeFormatting = true,
+			extraArgs = {},
+		},
 	}, opts)
 
 	lspconfig.rust_analyzer.setup(rust_analyzer_opts)
