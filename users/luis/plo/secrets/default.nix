ab2a0b0ce514fc74e160c5c3daa7f0590ded51bf
luis <luis@pleme.io>
2024-11-25 22:57:22 -0300

    + toolchain.toml

--- users/luis/plo/secrets/default.nix
+++ users/luis/plo/secrets/default.nix
@@ -2,7 +2,13 @@
   imports = [
     ./aws.nix
   ];
-
   sops.gnupg.home = "/home/luis/.gnupg";
   sops.gnupg.sshKeyPaths = [ ];
+  programs.gpg.enable = true;
+  services.gpg-agent.enable = true;
+  services.gpg-agent.pinentryPackage = pkgs.pinentry-curses;
+  services.gpg-agent.extraConfig = ''
+    default-cache-ttl 600
+    max-cache-ttl 7200
+  '';
 }
