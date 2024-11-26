4fda9ac6ac9703ba355b847576e5ab7454b1c464
luis <luis@pleme.io>
2024-11-26 09:55:56 -0300

    env for bash

--- nodes/plo/virtualisation.nix
+++ nodes/plo/virtualisation.nix
@@ -1,11 +1,11 @@
 { ... }: {
+  virtualisation.libvirtd.enable = false;
   virtualisation.podman.enable = false;
   virtualisation.podman.dockerSocket.enable = false;
   virtualisation.podman.defaultNetwork.settings.dns_enabled = false;
   virtualisation.docker.enable = true;
   virtualisation.docker.rootless = {
-    setSocketVariable = true;
-    enable = true;
+    setSocketVariable = false;
+    enable = false;
   };
-  virtualisation.libvirtd.enable = false;
 }
