c8b9020e9720fd3e83672542ff6d404c595abe62
luis <luis@pleme.io>
2024-11-30 16:47:25 -0300

    init

--- modules/home-manager/blackmatter/components/desktop/default.nix
+++ modules/home-manager/blackmatter/components/desktop/default.nix
@@ -8,7 +8,6 @@ in
     ./alacritty
     ./chrome
     ./firefox
-    ./gnome
     ./i3
     ./kitty
     ./packages
@@ -29,7 +28,6 @@ in
       blackmatter.components.desktop.alacritty.enable = true;
       blackmatter.components.desktop.chrome.enable = false;
       blackmatter.components.desktop.firefox.enable = false;
-      blackmatter.components.desktop.gnome.enable = false;
       blackmatter.components.desktop.i3.enable = true;
       blackmatter.components.desktop.kitty.enable = true;
       blackmatter.components.desktop.packages.enable = true;
