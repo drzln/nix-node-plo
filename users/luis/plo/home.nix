f12d684df46d82204b1716e804a6793e37da16c2
luis <luis@pleme.io>
2024-11-30 15:39:56 -0300

    init

--- users/luis/plo/home.nix
+++ users/luis/plo/home.nix
@@ -1,8 +1,8 @@
 { pkgs, ... }:
 {
   imports = [
-    ./packages.nix
     ./blackmatter.nix
+    ./packages.nix
     ./secrets
     ./shadeflakes
     ./kubernetes
--- users/luis/plo/kubernetes/default.nix
+++ users/luis/plo/kubernetes/default.nix
@@ -3,7 +3,7 @@
     requirements.outputs.homeManagerModules.blackmatter
   ];
 
-  blackmatter.kubernetes.enable = true;
-  blackmatter.kubernetes.k3d.enable = false;
-  blackmatter.kubernetes.k3d.client.enable = false;
+  # blackmatter.kubernetes.enable = true;
+  # blackmatter.kubernetes.k3d.enable = false;
+  # blackmatter.kubernetes.k3d.client.enable = false;
 }
