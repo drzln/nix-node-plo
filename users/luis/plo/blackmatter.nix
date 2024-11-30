feb0d64337ff14c4c22675bc56beb87d3047d7b4
luis <luis@pleme.io>
2024-11-30 14:41:39 -0300

    gaby gnome

--- users/luis/plo/blackmatter.nix
+++ users/luis/plo/blackmatter.nix
@@ -2,18 +2,11 @@
   imports = [
     requirements.outputs.homeManagerModules.blackmatter
   ];
+  blackmatter.gitconfig.email = "luis@pleme.io";
+  blackmatter.gitconfig.user = "luis";
+  blackmatter.desktop.enable = true;
   blackmatter.programs.nvim.enable = true;
   blackmatter.programs.nvim.package = pkgs.neovim_drzln;
   blackmatter.shell.enable = true;
   blackmatter.gitconfig.enable = true;
-  blackmatter.gitconfig.email = "luis@pleme.io";
-  blackmatter.gitconfig.user = "luis";
-  blackmatter.desktop.enable = true;
-  blackmatter.desktop.monitors = {
-    main = {
-      name = "DP-2"; # Secondary monitor output name
-      mode = "1920x1080"; # Resolution of the monitor
-      rate = "360"; # Refresh rate in Hz for a higher refresh secondary monitor
-    };
-  };
 }
