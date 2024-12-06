eb6ea6283da2de1eb6fad95f8ec3319bd078eb53
luis <luis@pleme.io>
2024-12-06 17:05:11 -0300

    generalize nixos for hyprland

--- modules/nixos/blackmatter/profiles/blizzard/default.nix
+++ modules/nixos/blackmatter/profiles/blizzard/default.nix
@@ -15,8 +15,8 @@ in
     ./virtualisation
     ./bluetooth
     ./sound
-		./networking
-		./nix
+    ./networking
+    ./nix
   ];
 
   options = {
@@ -32,62 +32,78 @@ in
   config = mkMerge [
     (mkIf (cfg.enable)
       {
+        powerManagement.cpuFreqGovernor = "performance";
+        environment.variables = {
+          GBM_BACKEND = "nvidia-drm";
+        };
+        services.dbus.enable = true;
+        services.udev.enable = true;
         services.printing.enable = false;
         services.hardware.bolt.enable = false;
         services.nfs.server.enable = false;
         security.rtkit.enable = true;
-        powerManagement.cpuFreqGovernor = "performance";
-
-        # Enable Wayland hardware acceleration (OpenGL)
-        # hardware.opengl.enable = true;
-
-        # Enable seatd for seat/session management, required by Hyprland
-        # services.seatd.enable = true;
-
-        # Input device configuration via libinput (Wayland-friendly)
-        # services.libinput.enable = true;
-
-        # Choose video drivers if necessary (adjust for your hardware)
-        # For Intel:
-        # services.xserver.videoDrivers = [ "intel" ];
-
-        # For AMD:
-        # services.xserver.videoDrivers = [ "amdgpu" ];
-
-        # For NVIDIA (proprietary):
-        # services.xserver.videoDrivers = [ "nvidia" ];
-
-        # Minimal Wayland display manager (greetd) setup
-        # services.greetd = {
-        #   enable = true;
-        #   # Using agreety (simple TUI greeter)
-        #   greeter = {
-        #     package = pkgs.agreety;
-        #   };
-        #   defaultSession = "hyprland";
-        #   sessions = [
-        #     {
-        #       name = "hyprland";
-        #       command = "${pkgs.hyprland}/bin/Hyprland";
-        #     }
-        #   ];
-        # };
-
-        # Enable pipewire and wireplumber for audio on Wayland
-        # services.pipewire = {
-        #   enable = true;
-        #   alsa.enable = true;
-        #   pulse.enable = true;
-        # };
-
-        # services.wireplumber.enable = true;
-
-        # Set your preferred login user
-        # NOTE: Replace 'your-username' with your actual username
-        # users.users.your-username = {
-        #   isNormalUser = true;
-        #   extraGroups = [ "wheel" "audio" "video" "input" ];
-        # };
+        services.seatd.enable = true;
+        services.xserver.videoDrivers = [ "nvidia" ];
+        console = { font = "Lat2-Terminus16"; keyMap = "us"; };
+        programs.zsh.enable = true;
+        services.libinput = { enable = true; };
+        xdg.portal.enable = true;
+        xdg.portal.wlr.enable = true;
+        hardware.graphics = {
+          opengl.enable = true;
+          nvidia = {
+            open = false;
+            modesettings.enable = true;
+          };
+          enable = true;
+          package = pkgs-unstable.mesa.drivers;
+          enable32Bit = true;
+          package32 = pkgs-unstable.pkgsi686Linux.mesa.drivers;
+          extraPackages = with pkgs; [
+            intel-media-driver
+            vaapiIntel
+            vaapiVdpau
+            libvdpau-va-gl
+          ];
+          extraPackages32 = with pkgs.pkgsi686Linux; [
+            vaapiIntel
+          ];
+        };
+        environment.systemPackages = with pkgs; [
+          requirements.inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland
+          xdg-desktop-portal-wlr
+          xdg-desktop-portal-gtk
+          vim
+          wget
+          git
+          bash
+          fontconfig
+        ];
+        fonts.fontconfig.enable = true;
+        fonts.fontDir.enable = true;
+        fonts.enableDefaultPackages = true;
+        fonts.packages = with pkgs; [
+          fira-code
+          fira-code-symbols
+          dejavu_fonts
+        ];
+        programs.hyprland.enable = false;
+        programs.hyprland.xwayland.enable = false;
+        programs.hyprland.package = requirements.inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+        programs.hyprland.portalPackage = requirements.inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+        services.greetd = {
+          enable = true;
+          greeter = {
+            package = pkgs.agreety;
+          };
+          defaultSession = "hyprland";
+          sessions = [
+            {
+              name = "hyprland";
+              command = "${pkgs.hyprland}/bin/Hyprland";
+            }
+          ];
+        };
       })
   ];
 }
--- nodes/plo/configuration.nix
+++ nodes/plo/configuration.nix
@@ -21,45 +21,13 @@ let
 in
 {
   system.stateVersion = "24.05";
+  nixpkgs.config.allowUnfree = true;
 
   imports = [
     requirements.outputs.nixosModules.blackmatter
   ];
 
-  console = { font = "Lat2-Terminus16"; keyMap = "us"; };
-
-  programs.zsh.enable = true;
-  services.libinput = { enable = true; };
-
   blackmatter.profiles.blizzard.enable = true;
-
-  hardware.nvidia.open = false;
-  hardware.graphics = {
-    enable = true;
-    package = pkgs-unstable.mesa.drivers;
-
-    # if you also want 32-bit support (e.g for Steam)
-    enable32Bit = true;
-    package32 = pkgs-unstable.pkgsi686Linux.mesa.drivers;
-    extraPackages = with pkgs; [
-      intel-media-driver
-      vaapiIntel
-      vaapiVdpau
-      libvdpau-va-gl
-    ];
-    extraPackages32 = with pkgs.pkgsi686Linux; [
-      vaapiIntel
-    ];
-  };
-
-  hardware.nvidia.modesetting.enable = true;
-
-  environment.variables = {
-    GBM_BACKEND = "nvidia-drm";
-  };
-
-  xdg.portal.enable = true;
-  xdg.portal.wlr.enable = true;
 
   users.users.luis = {
     uid = 1001;
@@ -76,40 +44,9 @@ in
     description = "gaby";
   } // sudo-users-common;
 
-
   security.sudo.extraConfig = ''
     luis ALL=(ALL) NOPASSWD:ALL
     gab ALL=(ALL) NOPASSWD:ALL
     gaby ALL=(ALL) NOPASSWD:ALL
   '';
-
-  nixpkgs.config.allowUnfree = true;
-
-  environment.systemPackages = with pkgs; [
-    requirements.inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland
-    xdg-desktop-portal-wlr
-    xdg-desktop-portal-gtk
-    vim
-    wget
-    git
-    bash
-    fontconfig
-  ];
-
-  programs.hyprland.enable = false;
-  programs.hyprland.xwayland.enable = false;
-  programs.hyprland.package = requirements.inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-  programs.hyprland.portalPackage = requirements.inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
-
-  services.dbus.enable = true;
-  services.udev.enable = true;
-
-  fonts.fontconfig.enable = true;
-  fonts.fontDir.enable = true;
-  fonts.enableDefaultPackages = true;
-  fonts.packages = with pkgs; [
-    fira-code
-    fira-code-symbols
-    dejavu_fonts
-  ];
 }
