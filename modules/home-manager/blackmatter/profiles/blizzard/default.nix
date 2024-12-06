aceaa240dba5b3a99b6627dfdffa82cc9656f53a
luis <luis@pleme.io>
2024-12-06 17:51:15 -0300

    generalize nixos for hyprland

--- modules/home-manager/blackmatter/profiles/blizzard/default.nix
+++ modules/home-manager/blackmatter/profiles/blizzard/default.nix
@@ -1,4 +1,4 @@
-{ lib, config, ... }:
+{ lib, config, pkgs, ... }:
 with lib;
 let
   cfg = config.blackmatter.profiles.blizzard;
@@ -26,10 +26,14 @@ in
     (mkIf (cfg.enable)
       {
         blackmatter.components.nvim.enable = true;
+        blackmatter.components.nvim.package = pkgs.neovim_drzln;
         blackmatter.components.shell.enable = true;
         blackmatter.components.desktop.enable = true;
         blackmatter.components.desktop.hyprland.enable = true;
         blackmatter.components.desktop.i3.enable = false;
+        blackmatter.components.kubernetes.enable = false;
+        blackmatter.components.kubernetes.k3d.enable = false;
+        blackmatter.components.kubernetes.k3d.client.enable = false;
       })
   ];
 }
--- users/luis/plo/blackmatter.nix
+++ users/luis/plo/blackmatter.nix
@@ -3,9 +3,10 @@
     requirements.outputs.homeManagerModules.blackmatter
   ];
   blackmatter.enable = true;
+
   blackmatter.profiles.winter.enable = false;
   blackmatter.profiles.blizzard.enable = true;
-  blackmatter.components.nvim.package = pkgs.neovim_drzln;
+
   blackmatter.components.desktop.i3.monitors = {
     main = {
       name = "DP-2";
@@ -13,10 +14,8 @@
       rate = "360";
     };
   };
+
+  blackmatter.components.gitconfig.enable = true;
   blackmatter.components.gitconfig.email = "luis@pleme.io";
   blackmatter.components.gitconfig.user = "luis";
-  blackmatter.components.gitconfig.enable = true;
-  blackmatter.components.kubernetes.enable = false;
-  blackmatter.components.kubernetes.k3d.enable = false;
-  blackmatter.components.kubernetes.k3d.client.enable = false;
 }
