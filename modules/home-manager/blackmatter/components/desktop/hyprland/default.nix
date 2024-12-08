5807513ac65e9d8b10666207a868424f94c930ec
luis <luis@pleme.io>
2024-12-08 19:24:45 -0300

    fbdev=1

--- modules/home-manager/blackmatter/components/desktop/hyprland/default.nix
+++ modules/home-manager/blackmatter/components/desktop/hyprland/default.nix
@@ -34,6 +34,14 @@ in
 
       # Manage the configuration
       home.file.".config/hypr/hyprland.conf".source = ./hyprland.conf;
+      home.file.".config/hypr/env.conf".source = ./env.conf;
+      home.file.".config/hypr/monitors.conf".source = ./monitors.conf;
+      home.file.".config/hypr/variables.conf".source = ./variables.conf;
+      home.file.".config/hypr/general.conf".source = ./general.conf;
+      home.file.".config/hypr/autostart.conf".source = ./autostart.conf;
+      home.file.".config/hypr/cursor.conf".source = ./cursor.conf;
+      home.file.".config/hypr/workspaces.conf".source = ./workspaces.conf;
+
       home.file.".config/hypr/wallpaper.jpg".source = ./wallpaper.jpg;
       # Optionally manage Waybar configuration
       home.file.".config/waybar/config".source = ./waybar-config.json;
