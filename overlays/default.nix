69d4a26ffe33bd2926987136bba853061248a2d3
luis <luis@pleme.io>
2024-12-09 04:48:05 -0300

    xdph

--- overlays/default.nix
+++ overlays/default.nix
@@ -5,21 +5,19 @@
   (self: super: {
     cb = super.buildGoPackage rec {
       pname = "cb";
-      version = "1.0.0"; # Replace with the actual version tag
+      version = "1.0.0";
 
-      # Fetch the source from GitHub
       src = super.fetchFromGitHub {
         owner = "niedzielski";
         repo = "cb";
-        rev = "v1.0.0"; # Replace with the desired commit or tag
+        rev = "main"; # Replace with the desired commit or tag
         sha256 = "sha256-OsHIacgorYnB/dPbzl1b6rYUzQdhTtsJYLsFLJxregk=";
       };
-      vendorHash = "sha256-OsHIacgorYnB/dPbzl1b6rYUzQdhTtsJYLsFLJxregk=";
 
       goPackagePath = "github.com/niedzielski/cb";
 
       buildInputs = with super; [
-				go
+        go
       ];
     };
   })
