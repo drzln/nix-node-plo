433306ec5f07dfbcde16ab09ed2f2d732766da34
luis <ldesiqueira@pinger.com>
2024-12-12 17:12:14 -0300

    docker for gcd

--- users/gabrielad/gcd/blackmatter.nix
+++ users/gabrielad/gcd/blackmatter.nix
@@ -9,11 +9,11 @@
   blackmatter.components.gitconfig.email = "gahb.dias@gmail.com";
   blackmatter.components.gitconfig.user = "gabrielad";
   blackmatter.components.desktop.enable = true;
-  blackmatter.components.desktop.monitors = {
-    main = {
-      name = "DP-2"; # Secondary monitor output name
-      mode = "1920x1080"; # Resolution of the monitor
-      rate = "360"; # Refresh rate in Hz for a higher refresh secondary monitor
-    };
-  };
+  # blackmatter.components.desktop.monitors = {
+  #   main = {
+  #     name = "DP-2"; # Secondary monitor output name
+  #     mode = "1920x1080"; # Resolution of the monitor
+  #     rate = "360"; # Refresh rate in Hz for a higher refresh secondary monitor
+  #   };
+  # };
 }
