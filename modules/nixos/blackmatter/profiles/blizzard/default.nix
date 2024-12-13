2ad3a6d1f50dd4696ca8c5f949214e6c355ef1bd
luis <luis@pleme.io>
2024-12-13 19:02:23 -0300

    init

--- modules/nixos/blackmatter/profiles/blizzard/default.nix
+++ modules/nixos/blackmatter/profiles/blizzard/default.nix
@@ -87,9 +87,7 @@ in
             default_session = {
               command = "
 ${pkgs.greetd.tuigreet}/bin/tuigreet \
-								--remember \
-								--remember-session \
-								--remember-user-session
+								--cmd hyprland
 ";
             };
           };
