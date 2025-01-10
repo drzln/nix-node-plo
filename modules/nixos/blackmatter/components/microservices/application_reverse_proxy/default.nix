de69380d8ca1a4e3e67774e44df3653592dca358
luis <luis@pleme.io>
2025-01-10 18:56:23 -0300

    application reverse proxy

--- modules/nixos/blackmatter/components/microservices/application_reverse_proxy/default.nix
+++ modules/nixos/blackmatter/components/microservices/application_reverse_proxy/default.nix
@@ -85,10 +85,10 @@ in
       services.consul = {
         enable = true;
         package = cfg.consul.package;
-        server = {
-          enabled = true;
-          bootstrapExpect = 1;
-        };
+        # server = {
+        #   enabled = true;
+        #   bootstrapExpect = 1;
+        # };
         # For demonstration: assign extraConfig even if not directly used by Consul module
         # This field may need special handling depending on how you intend to apply it.
         extraConfig = cfg.consul.extraConfig;
