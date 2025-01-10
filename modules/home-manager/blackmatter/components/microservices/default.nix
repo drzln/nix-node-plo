f314403a254bc1d460dfb3e40f75d1c3c5a4e8fc
luis <luis@pleme.io>
2025-01-10 18:03:32 -0300

    give config a schema

--- modules/home-manager/blackmatter/components/microservices/default.nix
+++ modules/home-manager/blackmatter/components/microservices/default.nix
@@ -12,32 +12,15 @@ in
   options = {
     blackmatter = {
       components = {
-        nvim = {
-          enable = mkEnableOption "Enable Neovim configuration";
-
-          package = mkOption {
-            type = types.package;
-            default = pkgs.neovim;
-            description = mdDoc "Neovim package/derivation";
-          };
+        microservices = {
+          enable = mkEnableOption "microservices";
         };
       };
     };
   };
 
+	# in case we do find anything global about microservices
   config = mkMerge [
-    (mkIf cfg.enable {
-      # Include the Neovim package
-      home.packages = [ cfg.package ];
-
-      # Configure Neovim files
-      xdg.configFile."nvim/init.lua".source = ./conf/init.lua;
-      xdg.configFile."nvim/lua/plugins/init.lua".source = ./conf/lua/plugins/init.lua;
-      xdg.configFile."nvim/lua/utils".source = ./conf/lua/utils;
-      xdg.configFile."nvim/lua/lsp".source = ./conf/lua/lsp;
-      xdg.configFile."nvim/after".source = ./conf/after;
-
-      blackmatter.components.nvim.plugin.groups.enable = true;
-    })
+    (mkIf cfg.enable { })
   ];
 }
