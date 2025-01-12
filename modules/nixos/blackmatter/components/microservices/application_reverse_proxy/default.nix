630aa557845f413f693327ec05313fe18369a0b4
luis <luis@pleme.io>
2025-01-12 09:30:23 -0300

    make d summon dmenu

--- modules/nixos/blackmatter/components/microservices/application_reverse_proxy/default.nix
+++ modules/nixos/blackmatter/components/microservices/application_reverse_proxy/default.nix
@@ -35,15 +35,11 @@ let
         default = pkgs.consul;
         description = mdDoc "Consul binary to use";
       };
-      # Optionally add more consul-related config here if you want
-      # to pass them along (e.g. port, mode, extraConfig, etc.)
     };
   };
 in
 {
   imports = [
-    # Import the existing consul module that defines
-    # blackmatter.components.microservices.consul
     ../consul
   ];
 
@@ -58,14 +54,11 @@ in
   };
 
   config = mkMerge [
-    # If the top-level reverse proxy + Traefik is enabled, configure Traefik.
     (mkIf (cfg.enable && cfg.traefik.enable) {
-      # Example: If you had a Traefik module, you’d pass the package here:
-      # services.traefik = {
-      #   enable = true;
-      #   package = cfg.traefik.package;
-      #   ...
-      # };
+      services.traefik = {
+        enable = true;
+        package = cfg.traefik.package;
+      };
     })
 
     # If the top-level reverse proxy + Consul is enabled, configure the lower-tier consul.
@@ -73,12 +66,6 @@ in
       blackmatter.components.microservices.consul = {
         enable = true;
         package = cfg.consul.package;
-        # If you want to pass additional settings to the lower-tier consul,
-        # like port or mode, you can do that here. For example:
-        #
-        # mode = "prod";           # or something else
-        # port = 8500;            # or something else
-        # extraConfig = { ... };  # etc.
       };
     })
   ];
