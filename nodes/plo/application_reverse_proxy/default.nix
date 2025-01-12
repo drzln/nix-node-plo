55f4f1c4f28ba8260cfaef5ec78ea45a48755f58
luis <luis@pleme.io>
2025-01-12 11:54:35 -0300

    init

--- nodes/plo/application_reverse_proxy/default.nix
+++ nodes/plo/application_reverse_proxy/default.nix
@@ -7,16 +7,19 @@
     enable = true;
     traefik.namespace = "plo";
     consul.namespace = "plo";
-    traefik.settings = ''
-      # Custom Traefik configuration
-      entryPoints:
-        web:
-          address: ":80"
-      providers:
-        file:
-          directory: /etc/traefik/custom/
-          watch: true
-    '';
+    traefik.settings = {
+      entryPoints = {
+        web = {
+          address = ":80";
+        };
+      };
+      providers = {
+        file = {
+          directory = "/etc/traefik/custom/";
+          watch = true;
+        };
+      };
+    };
     # consul.extraConfig = ''
     #   # Custom Consul configuration or notes
     #   # This configuration string is stored but may need specific handling in your setup.
